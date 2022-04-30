d003fa638e9a9b1f5f2f6c2e60728ddcc0801c5c
TechieDevice <45479102+TechieDevice@users.noreply.github.com>
2022-04-30 21:17:58 +0700

    fix file save error

--- EEG/splitter.py
+++ EEG/splitter.py
@@ -3,111 +3,237 @@ import mne
 import gc
 import pathlib
 
-totaldata = []
-nn = np.array([])
-dtype = [('Name', (np.str_, 15)), ('Max', np.float64), ('Min', np.float64), ('Avr', np.float64)]
-csv = np.array([], dtype=dtype)
-data_dir = pathlib.Path(__file__).parent.resolve()
-for item in data_dir.iterdir():
-    if (item.suffix == '.edf'):
-        name = item.stem
-        raw = mne.io.read_raw_edf(name+'.edf')
-        raw.crop().load_data()
-        e = mne.events_from_annotations(raw, use_rounding=False)
-        events = e[0]
-        dict = e[1]
-        print(events)
+inp = -1
+while inp != '0':
+    print('1 for numpy array, 2 for list, 0 for exit')
+    inp = input()
 
-        events_firsttouch = np.array([])
-        events_okbutton = np.array([])
-        first = dict['FirstTouch']
-        ok = dict['OkButton']
-        pad = 0
-        try:
-            pad = dict['Padding']
-        except:
-            pass
-        for j in range(events.shape[0]):
-            if (events[j,2] == first):
-                if (j != events.shape[0]-1):
-                    if (events[j+1,2] == ok):
-                        events_firsttouch = np.append(events_firsttouch, events[j,0])
-                        events_okbutton = np.append(events_okbutton, events[j+1,0])
-                else:
-                    events_firsttouch = np.append(events_firsttouch, events[j,0])
-                    if pad == 0:
-                        events_okbutton = np.append(events_okbutton, raw.times[raw.times.shape[0]-1]*125)
-                    else:
-                        events_okbutton = np.append(events_okbutton, events[j+1,0])
+    if inp == '1':
+        total_data = []
+        nn = []
+        dtype = [('Name', (np.str_, 15)), ('Max', np.float64), ('Min', np.float64), ('Avr', np.float64)]
+        csv = np.array([], dtype=dtype)
+        data_dir = pathlib.Path(__file__).parent.resolve()
+        for item in data_dir.iterdir():
+            if (item.suffix == '.edf'):
+                name = item.stem
+                raw = mne.io.read_raw_edf(name+'.edf')
+                raw.crop().load_data()
+                e = mne.events_from_annotations(raw, use_rounding=False)
+                events = e[0]
+                dict = e[1]
+                print(events)
 
-        print(events_firsttouch)
-        print(events_okbutton)
+                events_firsttouch = np.array([])
+                events_okbutton = np.array([])
+                first = dict['FirstTouch']
+                ok = dict['OkButton']
+                pad = 0
+                try:
+                    pad = dict['Padding']
+                except:
+                    pass
+                for j in range(events.shape[0]):
+                    if (events[j,2] == first):
+                        if (j != events.shape[0]-1):
+                            if (events[j+1,2] == ok):
+                                events_firsttouch = np.append(events_firsttouch, events[j,0])
+                                events_okbutton = np.append(events_okbutton, events[j+1,0])
+                        else:
+                            events_firsttouch = np.append(events_firsttouch, events[j,0])
+                            if pad == 0:
+                                events_okbutton = np.append(events_okbutton, raw.times[raw.times.shape[0]-1]*125)
+                            else:
+                                events_okbutton = np.append(events_okbutton, events[j+1,0])
 
-        s = events_okbutton - events_firsttouch
-        max = s.max()/125
-        min = s.min()/125
-        avr = (s.sum()/125)/s.shape[0]
+                print(events_firsttouch)
+                print(events_okbutton)
 
-        if s.shape[0] < 10:
-            continue
-        d = np.array([(name, max, min, avr)], dtype=dtype)
-        csv = np.concatenate((csv, d), axis=0)
+                s = events_okbutton - events_firsttouch
+                max = s.max()/125
+                min = s.min()/125
+                avr = (s.sum()/125)/s.shape[0]
 
-        data = np.array([])
-        read = False
-        i = 0
-        n = np.array([])
-        for j in range(raw._data.shape[1]):
-            if (j in events_firsttouch):
-                read = True
-                n = np.append(n, 0)
-            if (j in events_okbutton):
+                d = np.array([(name, max, min, avr)], dtype=dtype)
+                csv = np.concatenate((csv, d), axis=0)
+
+                file_data = np.array([])
                 read = False
-                i = i + 1
-            if (read):
-                data = np.append(data, raw._data[:,j])
-                n[i] = int(n[i] + 1)
+                i = 0
+                n = np.array([])
+                for j in range(raw._data.shape[1]):
+                    if (j in events_firsttouch):
+                        read = True
+                        n = np.append(n, 0)
+                    if (j in events_okbutton):
+                        read = False
+                        i = i + 1
+                    if (read):
+                        file_data = np.append(file_data, raw._data[:,j])
+                        n[i] = int(n[i] + 1)
         
-        data = np.reshape(data, (int(data.shape[0]/(8)), 8))
-        zerodata = np.array([])
+                file_data = np.reshape(file_data, (int(file_data.shape[0]/(8)), 8))
+                zero_file_data = np.array([])
+
+                p = 0
+                for j in range(i):
+                    for k in range(int(n.max())):
+                        if ((k + int(n[j])) < n.max()):
+                            zero_file_data = np.append(zero_file_data, np.zeros((8, 1)))
+                        else:
+                            zero_file_data = np.append(zero_file_data, file_data[p,:])
+                            p = p + 1
+
+                zero_file_data = np.reshape(zero_file_data, (int(n.max()*i), 8))
+                total_data.append(zero_file_data)
+                nn.append(n)
+
+        data_with_names = []
+        zero_total_data = np.array([])
+        data_names = np.array([])
+
+        names = csv[:]['Name']
+        nn_max = 0
+        for arr in nn:
+            if nn_max < arr.max(): nn_max = arr.max()
 
         p = 0
-        for j in range(i):
-            for k in range(int(n.max())):
-                if ((k + int(n[j])) < n.max()):
-                    zerodata = np.append(zerodata, np.zeros((8, 1)))
-                else:
-                    zerodata = np.append(zerodata, data[p,:])
-                    p = p + 1
-
-        zerodata = np.reshape(zerodata, (int(n.max()*i), 8))
-        totaldata.append(zerodata)
-        nn = np.append(nn, n)
-        nn = np.reshape(nn, (len(totaldata),n.shape[0]))
-
-
-zerototaldata = np.array([])
-p = 0
-for a in range(len(totaldata)):
-    td = np.array(totaldata[a])
-    p = 0
-    for j in range(i):
-        for k in range(int(nn.max())):
+        for a in range(len(total_data)):
+            td = np.array(total_data[a])
+            p = 0
             n = nn[a]
-            if ((k + int(n.max())) < nn.max()):
-                zerototaldata = np.append(zerototaldata, np.zeros((8, 1, 1)))
-            else:
-                zerototaldata = np.append(zerototaldata, td[p,:])
-                p = p + 1
+            for j in range(n.shape[0]):
+                data_names = np.append(data_names, names[a])
+                for k in range(int(nn_max)):
+                    if ((k + int(n.max())) < nn_max):
+                        zero_total_data = np.append(zero_total_data, np.zeros((8, 1, 1)))
+                    else:
+                        zero_total_data = np.append(zero_total_data, td[p,:])
+                        p = p + 1
 
-zerototaldata = np.reshape(zerototaldata, (len(totaldata), i, int(nn.max()), 8))
-print(zerototaldata)
+        list_len = 0
+        for arr in nn:
+            list_len = list_len + arr.shape[0]
+        zero_total_data = np.reshape(zero_total_data, (list_len, int(nn_max), 8))
+        
+        np.save("data_array", zero_total_data)
+        np.save("names_list", names)
+
+        max = csv[:]['Max'].max()
+        min = csv[:]['Min'].min()
+        avr = (csv[:]['Avr'].sum())/csv.shape[0]
+        d = np.array([("Статистика:", max, min, avr)], dtype=dtype)
+        csv = np.concatenate((csv, d), axis=0)
+        np.savetxt('data.csv', csv, delimiter='      ', fmt=['%s' , '%f', '%f', '%f'], header='Name                           Max                 Min                 Avr', comments='')
 
 
-np.save("data_array", zerototaldata)
-max = csv[:]['Max'].max()
-min = csv[:]['Min'].min()
-avr = (csv[:]['Avr'].sum())/csv.shape[0]
-d = np.array([("Статистика:", max, min, avr)], dtype=dtype)
-csv = np.concatenate((csv, d), axis=0)
-np.savetxt('test data.csv', csv, delimiter='      ', fmt=['%s' , '%f', '%f', '%f'], header='Name                           Max                 Min                 Avr', comments='')
+    if inp == '2':
+        total_data = []
+        nn = []
+        dtype = [('Name', (np.str_, 15)), ('Max', np.float64), ('Min', np.float64), ('Avr', np.float64)]
+        csv = np.array([], dtype=dtype)
+        data_dir = pathlib.Path(__file__).parent.resolve()
+        for item in data_dir.iterdir():
+            if (item.suffix == '.edf'):
+                name = item.stem
+                raw = mne.io.read_raw_edf(name+'.edf')
+                raw.crop().load_data()
+                e = mne.events_from_annotations(raw, use_rounding=False)
+                events = e[0]
+                dict = e[1]
+                print(events)
+
+                events_firsttouch = np.array([])
+                events_okbutton = np.array([])
+                first = dict['FirstTouch']
+                ok = dict['OkButton']
+                pad = 0
+                try:
+                    pad = dict['Padding']
+                except:
+                    pass
+                for j in range(events.shape[0]):
+                    if (events[j,2] == first):
+                        if (j != events.shape[0]-1):
+                            if (events[j+1,2] == ok):
+                                events_firsttouch = np.append(events_firsttouch, events[j,0])
+                                events_okbutton = np.append(events_okbutton, events[j+1,0])
+                        else:
+                            events_firsttouch = np.append(events_firsttouch, events[j,0])
+                            if pad == 0:
+                                events_okbutton = np.append(events_okbutton, raw.times[raw.times.shape[0]-1]*125)
+                            else:
+                                events_okbutton = np.append(events_okbutton, events[j+1,0])
+
+                print(events_firsttouch)
+                print(events_okbutton)
+
+                s = events_okbutton - events_firsttouch
+                max = s.max()/125
+                min = s.min()/125
+                avr = (s.sum()/125)/s.shape[0]
+
+                d = np.array([(name, max, min, avr)], dtype=dtype)
+                csv = np.concatenate((csv, d), axis=0)
+
+                file_data = np.array([])
+                read = False
+                i = 0
+                n = np.array([])
+                for j in range(raw._data.shape[1]):
+                    if (j in events_firsttouch):
+                        read = True
+                        n = np.append(n, 0)
+                    if (j in events_okbutton):
+                        read = False
+                        i = i + 1
+                    if (read):
+                        file_data = np.append(file_data, raw._data[:,j])
+                        n[i] = int(n[i] + 1)
+        
+                file_data = np.reshape(file_data, (int(file_data.shape[0]/(8)), 8))
+                zero_file_data = np.array([])
+
+                p = 0
+                for j in range(i):
+                    for k in range(int(n.max())):
+                        if ((k + int(n[j])) < n.max()):
+                            zero_file_data = np.append(zero_file_data, np.zeros((8, 1)))
+                        else:
+                            zero_file_data = np.append(zero_file_data, file_data[p,:])
+                            p = p + 1
+
+                zero_file_data = np.reshape(zero_file_data, (int(n.max()*i), 8))
+                total_data.append(zero_file_data)
+                nn.append(n)
+
+        total_data_to_file = []
+        p = 0
+        nn_max = 0
+        for arr in nn:
+            if nn_max < arr.max(): nn_max = arr.max()
+
+        for a in range(len(total_data)):
+            zero_total_data = np.array([])
+            td = np.array(total_data[a])
+            p = 0
+            n = nn[a]
+            for j in range(n.shape[0]):
+                for k in range(int(nn_max)):
+                    if ((k + int(n.max())) < nn_max):
+                        zero_total_data = np.append(zero_total_data, np.zeros((8, 1, 1)))
+                    else:
+                        zero_total_data = np.append(zero_total_data, td[p,:])
+                        p = p + 1
+            zero_total_data = np.reshape(zero_total_data, (n.shape[0], int(nn_max), 8))
+            total_data_to_file.append(zero_total_data)
+
+        print(total_data_to_file)
+        np.save("data_list", total_data_to_file)
+
+        max = csv[:]['Max'].max()
+        min = csv[:]['Min'].min()
+        avr = (csv[:]['Avr'].sum())/csv.shape[0]
+        d = np.array([("Статистика:", max, min, avr)], dtype=dtype)
+        csv = np.concatenate((csv, d), axis=0)
+        np.savetxt('data.csv', csv, delimiter='      ', fmt=['%s' , '%f', '%f', '%f'], header='Name                           Max                 Min                 Avr', comments='')
--- EEG/test.py
+++ EEG/test.py
@@ -14,7 +14,17 @@ from io import StringIO
 import pathlib
 import pandas as pd
 
+def max_in_list (List):
+    Max = 0
+    for arr in List:
+        if Max < arr.max(): Max = arr.max()
+    return Max
 
+def list_len (List):
+    Len = 0
+    for arr in List:
+        Len = Len + arr.shape[0]
+    return Len
 
 raw1 = np.array([[0, 1, 2, 0, 11, 22, 33, 0, 111, 222, 0, 2222, 2222, 3333, 1111, 0],
                  [0, 2, 1, 0, 22, 11, 33, 0, 111, 111, 0, 1111, 3333, 3333, 1111, 0],
@@ -24,9 +34,9 @@ raw2 = np.array([[0, 5, 5, 0, 44, 55, 0, 444, 666, 555, 444, 555, 0, 5555, 4444,
                  [0, 4, 5, 0, 55, 55, 0, 666, 555, 444, 666, 555, 0, 4444, 5555, 5555, 0],
                  [0, 5, 5, 0, 44, 44, 0, 444, 666, 666, 444, 444, 0, 4444, 4444, 6666, 0]])
 
-raw3 = np.array([[0, 7, 9, 7, 0, 99, 88, 99, 0, 777, 888, 0, 9999, 8888, 9999, 0],
-                 [0, 9, 8, 9, 0, 88, 88, 77, 0, 888, 888, 0, 7777, 7777, 9999, 0],
-                 [0, 7, 9, 8, 0, 77, 77, 99, 0, 777, 777, 0, 9999, 8888, 7777, 0]])
+raw3 = np.array([[0, 7, 9, 7, 0, 99, 88, 99, 0, 777, 888, 0],
+                 [0, 9, 8, 9, 0, 88, 88, 77, 0, 888, 888, 0],
+                 [0, 7, 9, 8, 0, 77, 77, 99, 0, 777, 777, 0]])
 
 events_firsttouch1 = np.array([1, 4, 8, 11])
 events_okbutton1 = np.array([3, 7, 10, 15])
@@ -34,15 +44,17 @@ events_okbutton1 = np.array([3, 7, 10, 15])
 events_firsttouch2 = np.array([1, 4, 7, 13])
 events_okbutton2 = np.array([3, 6, 12, 16])
 
-events_firsttouch3 = np.array([1, 5, 9, 12])
-events_okbutton3 = np.array([4, 8, 11, 15])
+events_firsttouch3 = np.array([1, 5, 9])
+events_okbutton3 = np.array([4, 8, 11])
 
 allraw = [raw1, raw2, raw3]
 allevents_firsttouch = np.array([events_firsttouch1, events_firsttouch2, events_firsttouch3])
 allevents_okbutton = np.array([events_okbutton1, events_okbutton2, events_okbutton3])
 
+names = ["raw1", "raw2", "raw3"]
+
 totaldata = []
-nn = np.array([])
+nn = []
 
 for iraw in range(3):
     raw = allraw[iraw]
@@ -78,22 +90,27 @@ for iraw in range(3):
 
     zerodata = np.reshape(zerodata, (int(n.max()*i), 3))
     totaldata.append(zerodata)
-    nn = np.append(nn, n) 
-    nn = np.reshape(nn, (len(totaldata),n.shape[0]))
+    nn.append(n)
 
+data_with_names = []
 zerototaldata = np.array([])
+data_names = np.array([])
 p = 0
+nn_max = int(max_in_list(nn))
 for a in range(len(totaldata)):
     td = np.array(totaldata[a])
     p = 0
-    for j in range(i):
-        for k in range(int(nn.max())):
-            n = nn[a]
-            if ((k + int(n.max())) < nn.max()):
+    n = nn[a]
+    for j in range(n.shape[0]):
+        data_names = np.append(data_names, names[a])
+        for k in range(nn_max):
+            if ((k + int(n.max())) < nn_max):
                 zerototaldata = np.append(zerototaldata, np.zeros((3, 1, 1)))
             else:
                 zerototaldata = np.append(zerototaldata, td[p,:])
                 p = p + 1
 
-zerototaldata = np.reshape(zerototaldata, (len(totaldata), i, int(nn.max()), 3))
-print(totaldata)
+zerototaldata = np.reshape(zerototaldata, (list_len(nn), nn_max, 3))
+data_with_names.append(zerototaldata)
+data_with_names.append(data_names)
+print(data_with_names)
